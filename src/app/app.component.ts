c46b5d9c4419adce24752729de5b9046c5b9a846
seb-panda <sebpanek@outlook.com>
2020-06-01 21:46:28 +0200

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,19 @@ export class AppComponent {
       this.dataSource = new MatTableDataSource(res as Array<XmlModel>);
     });
   }
+  openFile(event) {
+      var input = event.target;
+      console.log(input);
+    var text = "";
+      var reader = new FileReader();
+      var onload = function(event) {
+        this.text = reader.result;
+      };
+      console.log(this.text);
+      reader.onload = onload;
+      reader.readAsText(input.files[0]);
+    
+    };
   ngOnInit(){
     this.initItems();
   }
@@ -42,21 +55,56 @@ export class AppComponent {
     {surname: 'asd', name: 'Hydrogen', phone: '123123123123', city: 'H', birthDate: this.myDate, id: 1},
     {surname: 'asd', name: 'Hydrogen', phone: '123123123123', city: 'H', birthDate: this.myDate, id: 1}
   ];*/
-  openSnackBar(file : File[]) {
-    const files = (this.file.nativeElement as HTMLInputElement).files;
+  fileOutput;
+  public onChange11(event) {
+      var file = event.target.files[0];
+      if(!types.includes(file.type)){
+        this.openAlert('Typ pliku ' + file.name + ' nie jest akceptowany');
+        return;
+      }
+      else{
+        if (file.type == 'text/xml'){
+          var reader = new FileReader();
+          reader.onload = (e: any) => {
+            // The file's text will be printed here
+            this._service.addFile(e.target.result as File).subscribe(res => {this.openAlert(res as string);  console.log(res);   this.initItems(); }, err => {this.openAlert(err as string); console.log(err);});
+             
+            
+          };
+        }
+        else{
+          this._service.addFile(file as File).subscribe(res => {this.openAlert(res as string);  console.log(res);   this.initItems(); }, err => {this.openAlert(err as string); console.log(err);});
+
+        }
+        
+        
+      }
+  
+
+   
+  }
+  openSnackBar(file) {
+
+    /*const files = (this.file.nativeElement as HTMLInputElement).files;
     for (let i = 0; i < files.length; i++) {
+      console.log(files[i]);
       if(!types.includes(files[i].type)){
+        let reader = new FileReader();
+        reader.onload = () => {
+            // this 'text' is the content of the file
+            var text = reader.result;
+        }
         this.openAlert('Typ pliku ' + files[i].name + ' nie jest akceptowany');
         return;
       }
       else{
         this._service.addFile(files[i] as File).subscribe(res => {this.openAlert(res as string);  console.log(res);   this.initItems(); }, err => {this.openAlert(err as string); console.log(err);});
-      }
+      }*/
+
+       // this.initItems();
+
     }
-        this.initItems();
-
-
-  }
+  
   private openAlert(name : string){
     this._snackBar.open(name, 'Zamknij', {
       duration: 4000,
